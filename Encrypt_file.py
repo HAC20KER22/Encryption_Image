0b67a6a40ebd6b72838658e39f96802733c0501d
HAC20KER22 <114013658+HAC20KER22@users.noreply.github.com>
2023-05-01 20:48:37 +0530

    Update Encrypt_file.py

--- Encrypt_file.py
+++ Encrypt_file.py
@@ -1,37 +1,18 @@
-'''try:
+try:
     path = input(r'Enter the path of the file: ')
     key = int(input("ENter the key to encrypt: "))
     print("The path of the file: ",path)
     print("The key for encryption: ",key)
-    f = open(path,rb)
+    f = open(path,'rb')
     image = f.read()
     f.close()
     print("Yes")
     image = bytearray(image)
     for index,values in enumerate(image):
-        image[index] = values**k
+        image[index] = values^key
     f = open(path,'wb')
     f.write(image)
     f.close()
     print("Encryption done......")
 except Exception:
     print("Exception came up....",Exception.__name__)
-
-
-#r = open("d:\\CS Project\ACM CLUB\photo.jpeg",'r')
-'''
-path = input(r'Enter the path of the file: ')
-key = int(input("ENter the key to encrypt: "))
-print("The path of the file: ",path)
-print("The key for encryption: ",key)
-f = open(path,'rb')
-image = f.read()
-f.close()
-print("Yes")
-image = bytearray(image)
-for index,values in enumerate(image):
-    image[index] = values^key
-f = open(path,'wb')
-f.write(image)
-f.close()
-print("Encryption done......")
